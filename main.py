d4b793bc38060c861de4ca18db1c4d13318e3f0f
EhsanTgv <EhsanTgv@gmail.com>
2022-01-20 11:03:28 +0300

    Curses Attributes and Colors

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import curses
 from curses import wrapper
+import time
 
 
 def main(stdscr):
     stdscr.clear()
-    stdscr.addstr(0, 0, "Hello World!")
-    stdscr.refresh()
+    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_YELLOW)
+    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    BLUE_AND_YELLOW = curses.color_pair(1)
+    GREEN_AND_BLACK = curses.color_pair(2)
+
+    for i in range(100):
+        color = BLUE_AND_YELLOW
+        if i % 2 == 0:
+            color = GREEN_AND_BLACK
+
+        stdscr.addstr(f"Count: {i}", color)
+        stdscr.refresh()
+        time.sleep(0.1)
+
     stdscr.getch()
+
 
 wrapper(main)
