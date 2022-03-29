0977bf6bb77cda503b25bf106d8649905a74f75d
EhsanTgv <EhsanTgv@gmail.com>
2022-03-29 22:11:49 +0300

    Getting User Keystrokes

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 import curses
 from curses import wrapper
+from curses.textpad import Textbox, rectangle
 import time
 
 
 def main(stdscr):
-    stdscr.clear()
     curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_YELLOW)
     curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_WHITE)
     BLUE_AND_YELLOW = curses.color_pair(1)
     GREEN_AND_BLACK = curses.color_pair(2)
+    ORANGE_AND_WHITE = curses.color_pair(3)
 
-    pad = curses.newpad(100, 100)
-    stdscr.refresh()
+    x, y = 0, 0
+    while True:
+        key = stdscr.getkey()
+        if key == 'KEY_LEFT':
+            x -= 1
+        elif key == 'KEY_RIGHT':
+            x += 1
+        elif key == 'KEY_UP':
+            y -= 1
+        elif key == 'KEY_DOWN':
+            y += 1
 
-    for i in range(100):
-        for j in range(26):
-            char = chr(67 + j)
-            pad.addstr(char, GREEN_AND_BLACK)
-
-    for i in range(50):
-        pad.refresh(0, i, 5, 5, 10, 25)
-        time.sleep(0.1)
-
-    stdscr.getch()
+        stdscr.clear()
+        stdscr.addstr(y, x, "0")
+        stdscr.refresh()
 
 
 wrapper(main)
