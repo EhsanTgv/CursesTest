0e34bb5236b63a24ac9c27d7a51cf399ee58c19f
EhsanTgv <EhsanTgv@gmail.com>
2022-03-29 22:24:54 +0300

    Implement Textboxes and user input

--- main.py
+++ main.py
@@ -11,30 +11,19 @@ def main(stdscr):
     BLUE_AND_YELLOW = curses.color_pair(1)
     GREEN_AND_BLACK = curses.color_pair(2)
     ORANGE_AND_WHITE = curses.color_pair(3)
-    stdscr.nodelay(True)
 
-    x, y = 0, 0
-    string_x = 0
-    while True:
-        try:
-            key = stdscr.getkey()
-        except:
-            key = None
+    win = curses.newwin(3, 18, 2, 2)
+    box = Textbox(win)
+    rectangle(stdscr, 1, 1, 5, 20)
 
-        if key == 'KEY_LEFT':
-            x -= 1
-        elif key == 'KEY_RIGHT':
-            x += 1
-        elif key == 'KEY_UP':
-            y -= 1
-        elif key == 'KEY_DOWN':
-            y += 1
+    stdscr.refresh()
 
-        stdscr.clear()
-        string_x += 1
-        stdscr.addstr(x, string_x // 50, 'Hello World!')
-        stdscr.addstr(y, x, "0")
-        stdscr.refresh()
+    box.edit()
+    text = box.gather().strip().replace("\n", " ")
+
+    stdscr.addstr(10, 40, text)
+
+    stdscr.getch()
 
 
 wrapper(main)
